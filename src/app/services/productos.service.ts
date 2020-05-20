b1384df6a2ecfbf509ae9f4d02b7cdef98c6c1f5
Fary Trujillo <jtrujillo2307@gmail.com>
2020-05-20 10:26:54 -0500

    Fin Seccion 6

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Productos } from '../Interface/info.productos.interface';
+import { Productoidx } from '../Interface/productoidx.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductosService {
   cargando =  true;
-  productos: Productos[] = [];
+  productos: Productoidx[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProducto();
@@ -15,8 +15,8 @@ export class ProductosService {
 
   private cargarProducto() {
     this.http
-      .get('https://angular-html-329e2.firebaseio.com/productos.json')
-      .subscribe((resp: Productos[]) => {
+      .get('https://angular-html-329e2.firebaseio.com/productos_idx.json')
+      .subscribe((resp: Productoidx[]) => {
         this.productos= resp;
         this.cargando = false;
         console.log(this.productos);
